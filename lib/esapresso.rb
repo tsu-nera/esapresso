7df54a26814338cb97640cf3ceae57b9acbe95b5
tsu-nera <fox10225fox@gmail.com>
2018-11-19 04:12:32 +0900

    wip

--- lib/esapresso.rb
+++ lib/esapresso.rb
@@ -1,5 +1,30 @@
 require "esapresso/version"
 
+require "erb"
+
 module Esapresso
-  # Your code goes here...
+  class << self
+    def generate
+      template = File.read(File.expand_path("../templates/template.md.erb", __dir__))
+      erb = ERB.new(template, 0, "%-")
+
+      reports = {}
+
+      p title = daily_report_title
+      p body = result(erb, reports)
+
+    end
+
+    def result(erb, reports)
+      erb.result(binding)
+    end
+
+    def user_name
+      "tsu-nera".freeze  # TODO 環境変数からユーザ名を取得
+    end
+
+    def daily_report_title
+      Time.now.strftime("日報_%Y%m%d_#{user_name}").to_s
+    end
+  end
 end
